4ce106e5d76e4ed8574e7ae8a8743bde97261442
thiagobadini <thiagobardini@hotmail.com>
2021-03-05 09:34:11 -0500

    Change project name from surf-rescue to surf-rescue-api.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'surf-rescue'
+const mongooseBaseName = 'surf-rescue-api'
 
 // create the mongodb uri for development and test
 const database = {
